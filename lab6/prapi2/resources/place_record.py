0bdeaaab92600239201e0f18d3a2db4bef9cb2ff
IndikaKuma <ipkumarawd@yahoo.com>
2020-03-27 09:20:21 +0100

    api lab

--- lab6/prapi2/resources/place_record.py
+++ lab6/prapi2/resources/place_record.py
@@ -23,3 +23,17 @@ class PlaceRecord(Resource):
             if name == record["name"]:
                 return record, 200  # return 200 HTTP status code to indicate success
         return {"message": "Place record not found"}, 404  # return 404 HTTP status code to indicate resource not found
+
+
+# resource collection place records
+class PlaceRecords(Resource):
+    def post(self):
+        record_to_be_created = request.get_json(force=True)
+        name = record_to_be_created['name']
+        for record in placeRecords:
+            if name == record["name"]:
+                return {"message": "Record with name {} already exists".format(
+                    name)}, 500  # 500 Internal Server Error HTTP status code
+
+        placeRecords.append(record_to_be_created)
+        return record_to_be_created, 201  # 201 Created HTTP status code
