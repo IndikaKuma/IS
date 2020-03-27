12ccc047ad128aa49a782b869a4da0eaf17ccedb
IndikaKuma <ipkumarawd@yahoo.com>
2020-03-27 09:08:20 +0100

    api lab

---
+++ lab6/prapi2/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_restful import Api
+
+from resources.place_record import PlaceRecord
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(PlaceRecord, '/placerecords/<string:name>')
+
+app.run(host='0.0.0.0', port=5000, debug=True)
+# In the context of servers, 0.0.0.0 can mean "all IPv4 addresses on the local machine".
