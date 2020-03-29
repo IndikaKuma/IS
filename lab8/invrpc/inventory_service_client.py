4875bcf02296fd95511122fc1e969c66d7171a84
IndikaKuma <ipkumarawd@yahoo.com>
2020-03-29 15:39:37 +0200

    Add the gPRC example

---
+++ lab8/invrpc/inventory_service_client.py
@@ -0,0 +1,29 @@
+from __future__ import print_function
+import logging
+
+import grpc
+
+import inventory_service_pb2
+import inventory_service_pb2_grpc
+
+
+def generate_product_list():
+    yield inventory_service_pb2.ProductType(type="Laptop")
+    yield inventory_service_pb2.ProductType(type="Phone")
+
+
+def run():
+    channel = grpc.insecure_channel('localhost:50051')
+    stub = inventory_service_pb2_grpc.InventoryServiceStub(channel)
+    response = stub.GetProductQuantity(inventory_service_pb2.ProductType(type="Laptop"))
+    print("Quantity for Product Type Laptop : " + str(response.amount))
+
+    product_list = generate_product_list()
+    response = stub.GetStockSummary(product_list)
+    print("Quantity for Product Type Laptop : " + str(response.productStocks["Laptop"].amount))
+    print("Quantity for Product Type Phone : " + str(response.productStocks["Phone"].amount))
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    run()
