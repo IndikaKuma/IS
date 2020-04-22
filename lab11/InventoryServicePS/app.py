1b6586bfad4acf986fd4f2d55e7de5e46e9b75f4
IndikaKuma <ipkumarawd@yahoo.com>
2020-04-22 14:36:35 +0200

    update lab 11

--- lab11/InventoryServicePS/app.py
+++ lab11/InventoryServicePS/app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_restful import Api
 
+from message_puller import MessagePuller
 from resources.product import Product
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Product, '/products/<string:pname>/quantity', methods=['GET','PUT'])
-
-app.run(host='0.0.0.0', port=5000, debug=True)
+MessagePuller()
+app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
 # In the context of servers, 0.0.0.0 can mean "all IPv4 addresses on the local machine".
--- lab11/InventoryServicePS/message_puller.py
+++ lab11/InventoryServicePS/message_puller.py
@@ -7,7 +7,8 @@ import requests
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
     payload = json.loads(body.decode('utf-8'))
-    msg = requests.post("http://127.0.0.1:5002/orders/", json=payload)
+    msg = \
+        requests.put("http://127.0.0.1:5000/products/"+payload['product type']+"/quantity?value=" + str(payload['quantity']))
     print(msg.content)
 
 
@@ -20,9 +21,9 @@ def pull_message():
     result = channel.queue_declare('', exclusive=True)
     queue_name = result.method.queue
 
-    channel.queue_bind(exchange='order', queue=queue_name, routing_key="order.create.*.*")
+    channel.queue_bind(exchange='order', queue=queue_name, routing_key="*.*.inventory.update")
 
-    print(' [*] Waiting for messages. To exit press CTRL+C ' + queue_name)
+    print(' [*] Waiting for messages. To exit press CTRL+C')
 
     channel.basic_consume(
         queue=queue_name, on_message_callback=callback, auto_ack=True)
