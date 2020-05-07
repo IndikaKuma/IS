5830cbd77cfb8f252efe409ae2cac54893e8cc6c
IndikaKuma <ipkumarawd@yahoo.com>
2020-05-07 21:17:51 +0200

    Lab 13 Update req.text

--- lab13/openapiplacerecordv3/app.py
+++ lab13/openapiplacerecordv3/app.py
@@ -6,7 +6,6 @@ from connexion.resolver import RestyResolver
 logging.basicConfig(level=logging.INFO)
 app = connexion.App(__name__, specification_dir="openapi/")
 app.add_api('placerecord-api.yaml',
-            arguments={'title': 'Place Record API'},
-            resolver=RestyResolver('api'))
+            arguments={'title': 'Place Record API'})
 
 app.run(host='0.0.0.0', port=5000, debug=True)
